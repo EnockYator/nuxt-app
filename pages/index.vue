32b66f0441d1c419eae44c01533cd135d1b5905b
EnockYator <ekyator02@gmail.com>
2024-07-13 18:58:06 +0300

    configured tailwind

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore voluptatem
+            dicta, obcaecati possimus illum vel adipisci harum earum iure.
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore voluptatem
+            dicta, obcaecati possimus illum vel adipisci harum earum iure.
+        </p>
+    </div>
+</template>
+
+<script setup>
+
+
+
+</script>
+
+<style scoped>
+p{
+    margin: 20px 0;
+}
+
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Product Details for {{id}}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore voluptatem
+            dicta, obcaecati possimus illum vel adipisci harum earum iure.
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore voluptatem
+            dicta, obcaecati possimus illum vel adipisci harum earum iure.
+        </p>
+    </div>
+</template>
+
+<script setup>
+const {id} = useRoute().params;
+
+definePageMeta ({
+    layout: 'products'
+});
+</script>
+
+<style scoped>
+
+
+</style>
